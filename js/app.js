419750e345928fc8be50d72d85481d2c1045cc9e
gerges-sheta <gergessgeta@ahoo.com>
2023-05-24 01:13:57 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    // HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPuase: false,
+        navText: ['PREV', 'NEXT'],
+        dots: false,
+        responsive: {
+            0: {
+
+                nav: false
+
+            },
+            768: {
+                nav: true
+            }
+        }
+    })
+
+
+    // PROJECT SLIDER
+    $('#project-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        smartSpeed: 1000,
+        margin: 24,
+        navText: ["PREV", "NEXT"],
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                margin: 0,
+            },
+            768: {
+                items: 2,
+            },
+            1140: {
+                items: 2,
+                center: true,
+                dots: true,
+            }
+        }
+    })
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        items: 1,
+        smartSpeed: 800,
+
+    })
+})
+
+
